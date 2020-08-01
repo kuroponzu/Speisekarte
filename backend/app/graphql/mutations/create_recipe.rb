e20df9663775bac6aa28d7fe80c5a24919c42c94
sho shimohata <pontatanpo1014@gmail.com>
2020-08-01 17:20:10 +0900

    add create recipe mutation

---
+++ backend/app/graphql/mutations/create_recipe.rb
@@ -0,0 +1,26 @@
+module Mutations
+  class CreateRecipe < BaseMutation
+    graphql_name 'CreateRecipe'
+
+    field :recipe, Types::RecipeType, null: true
+    field :result, Boolean, null: true
+
+    argument :title, String, required: false
+    argument :description, String, required: false
+    argument :cooking_time, Integer, required: false
+
+    def resolve(**args)
+      binding.pry
+      puts args
+      recipe = Recipe.create(
+        title: args[:title],
+        description: args[:description],
+        cooking_time: args[:cooking_time],
+      )
+      {
+        recipe: recipe,
+        result: recipe.errors.blank?
+      }
+    end
+  end
+end
