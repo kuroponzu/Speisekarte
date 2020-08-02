00d01b78562c93ded36ec6e9fd1d74ccbf746071
sho shimohata <pontatanpo1014@gmail.com>
2020-08-03 01:41:48 +0900

    create recipe screen

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,97 +1,132 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-container>
+    <v-row>
+      <v-col cols="6" offset="3">
+        <v-card>
+          <v-card-title>Register Recipes</v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-container>
+                <v-row>
+                  <v-col cols="6">
+                    <v-text-field v-model="newRecipe.title" label="Title" required />
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field v-model="newRecipe.description" label="Description" required />
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field v-model.number="newRecipe.cookingTime" label="cookingTime" required />
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col>
+                    <v-btn @click="createRecipe(newRecipe)">
+                      Register
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row v-for="recipe in recipes" :key="recipe.id">
+      <v-col cols="6" offset="3">
+        <v-card>
+          <v-card-title>
+            <v-btn text icon @click="deleteRecipe(recipe)">
+              <v-icon>clear</v-icon>
+            </v-btn>
+          </v-card-title>
+          <v-card-text>{{ recipe.title }}</v-card-text>
+          <v-card-text>{{ recipe.description }}</v-card-text>
+          <v-card-text>{{ recipe.cookingTime }}</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import recipes from '~/apollo/queries/recipes'
+import createRecipe from '~/apollo/mutations/createRecipe'
+import deleteRecipe from '~/apollo/mutations/deleteRecipe'
+import updateRecipe from '~/apollo/mutations/updateRecipe'
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data() {
+    return {
+      recipes: {},
+      newRecipe: {
+        title: '',
+        description: '',
+        cookingTime: 0,
+      }
+    }
+  },
+  methods: {
+    async createRecipe(recipe) {
+
+      try {
+        await this.$apollo.mutate({
+          mutation: createRecipe,
+          variables: {
+            id: recipe.id,
+            title: recipe.title,
+            description: recipe.description,
+            cookingTime: 10,
+          },
+          refetchQueries: [{
+            query: recipes
+          }]
+        })
+        this.newRecipe = { title: '', description: '', cookingTime: '' }
+      } catch (e) {
+        window.console.log(e)
+      }
+    },
+    async deleteRecipe(recipe) {
+      try {
+        await this.$apollo.mutate({
+          mutation: deleteRecipe,
+          variables: {
+            id: Number(recipe.id)
+          },
+          refetchQueries: [{
+            query: recipes
+          }]
+        })
+      } catch (e) {
+        window.console.log(e)
+      }
+    },
+    async updateRecipe(recipe) {
+      try {
+        await this.$apollo.mutate({
+          mutation: updateRecipe,
+          variables: {
+            id: recipe.id,
+            title: recipe.title,
+            description: recipe.description
+          },
+          refetchQueries: [{
+            query: recipes
+          }]
+        })
+      } catch (e) {
+        window.console.log(e)
+      }
+    }
+  },
+  apollo: {
+    recipes: {
+      query: recipes
+    }
   }
 }
 </script>
