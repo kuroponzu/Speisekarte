9a5e0fb5f4f10687ab67dd0ffed7aca61443bc87
sho shimohata <pontatanpo1014@gmail.com>
2020-08-10 22:40:11 +0900

    add find query

--- backend/app/graphql/types/query_type.rb
+++ backend/app/graphql/types/query_type.rb
@@ -4,5 +4,12 @@ module Types
     def recipes
       Recipe.all
     end
+
+    field :recipe, Types::RecipeType, null: false, description: 'レシピを一件取得する' do
+      argument :id, Int, required: true
+    end
+    def recipe(id:)
+      Recipe.find(id)
+    end
   end
 end
