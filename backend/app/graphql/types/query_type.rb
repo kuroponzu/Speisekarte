8fc56691362b79c7df3fd65200b6029da12cc0fd
sho shimohata <pontatanpo1014@gmail.com>
2020-08-10 22:54:11 +0900

    add serch_recipes_by_cooking_time

--- backend/app/graphql/types/query_type.rb
+++ backend/app/graphql/types/query_type.rb
@@ -11,5 +11,12 @@ module Types
     def recipe(id:)
       Recipe.find(id)
     end
+
+    field :serch_recipes_by_cooking_time, Types::RecipeType, null: false, description: '調理時間を指定し、レシピを一件取得する' do
+      argument :cooking_time, Int, required: true
+    end
+    def serch_recipes_by_cooking_time(cooking_time:)
+      Recipe.where(cooking_time: cooking_time).shuffle.first
+    end
   end
 end
